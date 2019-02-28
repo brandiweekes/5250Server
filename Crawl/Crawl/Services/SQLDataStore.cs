43daa7637a831ee353caa06171bd78b02d1b2110
brandiweekes <brandiweekes@gmail.com>
2019-02-28 15:06:26 -0800

    hook up item for SQL (#4)

--- Crawl/Crawl/Services/SQLDataStore.cs
+++ Crawl/Crawl/Services/SQLDataStore.cs
@@ -124,7 +124,21 @@ namespace Crawl.Services
 
         public async Task<bool> InsertUpdateAsync_Item(Item data)
         {
-            // Implement
+            // Check to see if the item exist
+            var oldData = await GetAsync_Item(data.Id);
+            if (oldData == null)
+            {
+                await AddAsync_Item(data);
+                return true;
+            }
+
+            // Compare it, if different update in the DB
+            var UpdateResult = await UpdateAsync_Item(data);
+            if (UpdateResult)
+            {
+                await AddAsync_Item(data);
+                return true;
+            }
 
             return false;
         }
